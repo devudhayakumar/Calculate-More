838cc88f61dba10a49d44aef6146c257c6f4c8ec
Udhayakumar T <60465112+dev-udhayakumar@users.noreply.github.com>
2021-10-12 19:19:23 +0530

    initial commit

--- app/src/main/java/dev/udhayakumar/calculator/MainActivity.java
+++ app/src/main/java/dev/udhayakumar/calculator/MainActivity.java
@@ -2,7 +2,10 @@ package dev.udhayakumar.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button agecal = (Button)findViewById(R.id.buttonswitch2);
+        Button cgpacal = (Button)findViewById(R.id.buttonswitch1);
+        cgpacal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent( MainActivity.this, CgpaCalculator.class);
+                startActivity(i);
+            }
+        });
+        agecal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent( MainActivity.this, AgeCalculator.class);
+                startActivity(i);
+            }
+        });
+
     }
+
 }
